d5425fa9715e4ead532b8c7cf96a019b71179146
weslei <wesleicasali18@gmail.com>
2023-07-26 10:07:15 -0300

    att

---
+++ app.js
@@ -0,0 +1,28 @@
+//MASCARA INPUT TEL
+document.getElementById('tel-form').addEventListener('keyup', (e)=>{
+    let input = e.target
+    input.value = phoneMask(input.value)
+})
+
+
+const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+}
+
+
+
+
+
+//------------REMOVER LOADER O MOSTRAR CONTENT APÓS INSERIR TODOS OS DADOS------------------
+function inserirDados(){
+    setTimeout(() => {
+        document.getElementById("loader").classList.remove('d-flex')
+        document.getElementById("loader").classList.add('d-none')
+        document.getElementById("all-content").classList.remove('d-none')
+    }, 2000);
+}
+inserirDados();
